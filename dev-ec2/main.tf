eb799c0ef88d855b37515a9f2aec4def16454f70
Mohamed Habib <moe.habib9@gmail.com>
2024-06-05 17:15:06 +0100

    Update main.tf

--- dev-ec2/main.tf
+++ dev-ec2/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+    backend "s3" {
+      bucket = "digger-states-test"
+      key    = "demo-ee/features/state"
+      region = "us-east-1"
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region
+}
+
 
 resource "aws_ssm_parameter" "foo" {
   name  = "/dev/ec2"
