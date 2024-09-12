1aa133693896c5ce1c0533ca64b4a0981c9e6ed0
Mohamed Habib <moe.habib9@gmail.com>
2024-09-12 16:46:31 +0100

    Update main.tf

--- prod-vpc/main.tf
+++ prod-vpc/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "foo" {
-  name  = "/prod/vpc"
-  type  = "String"
-  value = "11.10.10.0/32"
-}
+#resource "aws_ssm_parameter" "foo" {
+#  name  = "/prod/vpc"
+#  type  = "String"
+#  value = "11.10.10.0/32"
+#}
